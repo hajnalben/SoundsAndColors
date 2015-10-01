4b7c08ceafeafeb49a5f2b13c66796207706e672
Tok Dániel <tokdaniel4@gmail.com>
2015-10-01 23:25:02 +0200

    kezdetleges dizájn

--- app/scripts/services/aggregationService.js
+++ app/scripts/services/aggregationService.js
@@ -10,10 +10,11 @@
 angular.module('soundsAndColorsApp')
     .service('aggregationService', ['$http', 'colors', 'files',
         function($http, colors, files) {
-            // AngularJS will instantiate a singleton by calling "new" on this function
 
             function handle(response) {
-                if (!response.fileColorAggs)
+                if (!response.fileColorAggs) {
+                    return null;
+                }
 
                 var tmpAggs = new Array(files.length);
 
@@ -55,13 +56,13 @@ angular.module('soundsAndColorsApp')
 
                     console.log(response);
 
-                    return handle(JSON.parse(response));
+                    return handle(response);
 
                 }, function(response) {
                     // called asynchronously if an error occurs
                     // or server returns response with an error status.
 
-                    console.log("Error. Showing dummy data!")
+                    console.log('Error. Showing dummy data!');
 
                     response = JSON.parse('{"count":3,"fileColorAggs":[{"file":"a.m4a","color":"#000000","count":"1"},{"file":"a.m4a","color":"#008000","count":"2"},{"file":"b.m4a","color":"#0000ff","count":"1"},{"file":"b.m4a","color":"#c0c0c0","count":"1"},{"file":"b.m4a","color":"#ffff00","count":"1"},{"file":"c.m4a","color":"#00ffff","count":"1"},{"file":"c.m4a","color":"#c0c0c0","count":"1"},{"file":"c.m4a","color":"#ff0000","count":"1"},{"file":"d.m4a","color":"#800080","count":"1"},{"file":"d.m4a","color":"#ff0000","count":"1"},{"file":"d.m4a","color":"#ffff00","count":"1"},{"file":"e.m4a","color":"#808000","count":"2"},{"file":"e.m4a","color":"#c0c0c0","count":"1"},{"file":"f.m4a","color":"#000000","count":"1"},{"file":"f.m4a","color":"#008000","count":"1"},{"file":"f.m4a","color":"#00ffff","count":"1"},{"file":"g.m4a","color":"#ff0000","count":"1"},{"file":"g.m4a","color":"#ffff00","count":"1"},{"file":"g.m4a","color":"#ffffff","count":"1"},{"file":"h.m4a","color":"#00ffff","count":"2"},{"file":"h.m4a","color":"#808000","count":"1"},{"file":"i.m4a","color":"#808000","count":"1"},{"file":"i.m4a","color":"#a52a2a","count":"1"},{"file":"i.m4a","color":"#c0c0c0","count":"1"},{"file":"j.m4a","color":"#008000","count":"1"},{"file":"j.m4a","color":"#808000","count":"1"},{"file":"j.m4a","color":"#ffff00","count":"1"},{"file":"k.m4a","color":"#a52a2a","count":"1"},{"file":"k.m4a","color":"#ff0000","count":"1"},{"file":"k.m4a","color":"#ff00ff","count":"1"},{"file":"l.m4a","color":"#800080","count":"1"},{"file":"l.m4a","color":"#808000","count":"1"},{"file":"l.m4a","color":"#c0c0c0","count":"1"},{"file":"m.m4a","color":"#0000ff","count":"1"},{"file":"m.m4a","color":"#008000","count":"1"},{"file":"m.m4a","color":"#ffff00","count":"1"}]}');
 
